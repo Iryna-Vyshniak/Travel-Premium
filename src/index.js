cd09eeb0dfbb2b785c5880509ff9f8f5020539e5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-02 02:18:45 +0200

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import csv from './assets/people-100.csv';
 import './babel';
 import './styles/style.css';
 import './main.scss';
+import Lake from './assets/images/hero/lake-desktop.png';
+import Arrow from './assets/images/top-rated/arrow@2x-desktop.png';
+import Clouds from './assets/images/hero/clouds-desktop.png';
+import Call from './assets/images/menu/button-call-me-desktop.png';
 
 const post = new Post('Webpack Post Title', WebpackLogo);
 console.log('Post to String', post.toString());
