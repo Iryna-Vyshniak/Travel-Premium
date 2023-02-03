141ae4f74414766716269a47ea984ff58966d3de
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-03 21:38:15 +0200

    add header, complete hero structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Post from '@models/Post';
 
-import Logo from './assets/images/menu/logo@2x-desktop.png';
+import Logo from './assets/images/menu/logo-desktop.png';
 import './babel';
 import './styles/style.css';
 import './main.scss';
 import Lake from './assets/images/hero/lake@2x-desktop.png';
+import Lakes from './assets/images/hero/lakes@2x-desktop.png';
 // import Arrow from './assets/images/top-rated/arrow@2x-desktop.png';
 // import Clouds from './assets/images/hero/clouds-desktop.png';
 // import Call from './assets/images/menu/button-call-me-desktop.png';
