052232e32e1ce9c51841a21eb0547ea68eef107f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-03 18:50:53 +0200

    add logo and first bg hero

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import Post from '@models/Post';
 import json from './assets/json';
-import WebpackLogo from './assets/images/webpack-logo.png';
+import Logo from './assets/images/menu/logo@2x-desktop.png';
 import xml from './assets/data.xml';
 import csv from './assets/people-100.csv';
 import './babel';
 import './styles/style.css';
 import './main.scss';
-import Lake from './assets/images/hero/lake-desktop.png';
-import Arrow from './assets/images/top-rated/arrow@2x-desktop.png';
-import Clouds from './assets/images/hero/clouds-desktop.png';
-import Call from './assets/images/menu/button-call-me-desktop.png';
+import Lake from './assets/images/hero/lake@2x-desktop.png';
+// import Arrow from './assets/images/top-rated/arrow@2x-desktop.png';
+// import Clouds from './assets/images/hero/clouds-desktop.png';
+// import Call from './assets/images/menu/button-call-me-desktop.png';
 
 const post = new Post('Webpack Post Title', WebpackLogo);
 console.log('Post to String', post.toString());
