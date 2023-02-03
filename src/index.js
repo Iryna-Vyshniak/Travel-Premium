7b46a9c688a60373cce0a31615d52d8a33921883
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-03 19:03:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Post from '@models/Post';
-import json from './assets/json';
+
 import Logo from './assets/images/menu/logo@2x-desktop.png';
-import xml from './assets/data.xml';
-import csv from './assets/people-100.csv';
 import './babel';
 import './styles/style.css';
 import './main.scss';
@@ -11,8 +9,5 @@ import Lake from './assets/images/hero/lake@2x-desktop.png';
 // import Clouds from './assets/images/hero/clouds-desktop.png';
 // import Call from './assets/images/menu/button-call-me-desktop.png';
 
-const post = new Post('Webpack Post Title', WebpackLogo);
+const post = new Post('Webpack Post Title', Logo);
 console.log('Post to String', post.toString());
-console.log('JSON: ', json);
-console.log('XML: ', xml);
-console.log('CSV: ', csv);
